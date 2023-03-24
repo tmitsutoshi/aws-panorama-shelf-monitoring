9102e0ce71b2a6cf38f51860c7130227e15d895d
Mitsutoshi Taira <tairmits@amazon.co.jp>
2023-03-24 23:47:00 +0900

    Add 'StreamUri' Parameter

--- cloudformation/custom_resources/sqsInvokedFunction/lambda_function.py
+++ cloudformation/custom_resources/sqsInvokedFunction/lambda_function.py
@@ -8,10 +8,11 @@ gql_client = gql_resource.client()
 
 gql_query = """
 mutation MyMutation($s3Uri: String, $ProductType: ProductType = BOTTLE, $count: Int) {
-  updateShelfMonitor(input: {s3Uri: $s3Uri, ProductType: $ProductType, count: $count}) {
+  updateShelfMonitor(input: {s3Uri: $s3Uri, StreamUri: $StreamUri, ProductType: $ProductType, count: $count}) {
       count
       Threshold
       s3Uri
+      StreamUri
       ProductType
       createdAt
       updatedOn
@@ -21,10 +22,11 @@ mutation MyMutation($s3Uri: String, $ProductType: ProductType = BOTTLE, $count: 
 
 create_bottle_query = """
 mutation MyMutation($s3Uri: String, $ProductType: ProductType = BOTTLE, $Threshold: 3, $count: Int) {
-    createShelfMonitor(input: {s3Uri: $s3Uri, ProductType: $ProductType, count: $count}) {
+    createShelfMonitor(input: {s3Uri: $s3Uri, StreamUri: $StreamUri, ProductType: $ProductType, count: $count}) {
         count
         Threshold
         s3Uri
+        StreamUri
         ProductType
         createdAt
         updatedOn
@@ -40,13 +42,14 @@ def handler(event, context):
         payload = json.loads(record["body"])
         product_type = payload["ProductType"]
         product_count = payload["StockCount"]
+        stream_uri = payload["StreamUri"]
         bucket, key = payload["S3Uri"].replace("s3://", "").split("/", 1)
 
         presigned_url = s3.generate_presigned_url(
             "get_object", Params={"Bucket": bucket, "Key": key}, ExpiresIn=3600
         )
 
-        values = {"s3Uri": presigned_url, "count": product_count}
+        values = {"s3Uri": presigned_url, "StreamUri": stream_uri ,"count": product_count}
 
         try:
             mutation = gql_client.execute(
